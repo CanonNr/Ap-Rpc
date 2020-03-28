14b1dcd7648985cf0f94a34f9e8cabaaf6ade709
canon1014 <717437709@qq.com>
2020-03-28 09:43:33 +0800

    结构的优化

--- src/Server.php
+++ src/Server.php
@@ -3,5 +3,31 @@ namespace Canon\Rpc;
 
 class Server
 {
+    public $host;
 
+    public $port;
+
+    public $config;
+
+    public $services = [];
+
+    public function __construct($host,$port,$config)
+    {
+        $this->host = $host;
+        $this->port = intval($port);
+        $this->config = $config;
+        $this->isExistSwoole();
+    }
+
+    function isExistSwoole(){
+        if (!extension_loaded('swoole')) {
+            echo 'Swoole extension not installed';
+            die();
+        }
+    }
+
+    public function start()
+    {
+
+    }
 }
